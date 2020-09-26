f6925fb8b882caf3087756ac15deb4dbfbfbc4fd
Dar Hershkovits <darhers89@gmail.com>
2020-09-26 01:00:51 -0400

    create input field which outputs length of entered text below it in a paragraph

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    inputLength: 0
+  }
+
+  getInputLengthHandler = (event) => {
+    this.setState({
+      inputLength: event.target.value.length
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input 
+          type="text" 
+          onChange={(event) => {this.getInputLengthHandler(event)}}
+          placeholder="Enter your text here!"/>
+        <p>{this.state.inputLength}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
