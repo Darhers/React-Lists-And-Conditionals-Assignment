0e3c8c6960cbfcd036ec5499e1f3e1eddca26860
Dar Hershkovits <darhers89@gmail.com>
2020-09-26 01:09:36 -0400

    add conditional output to validation depending on input length

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Validation from './Validation/Validation.js'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const validation = (props) => {
     return (
-        <p>{props.length}</p>
+        <p>{props.length<5 ? "Text too short!" : "Text long enough!"}</p>
     );
 }
 
